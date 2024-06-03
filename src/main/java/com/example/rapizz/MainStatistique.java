219f5f701f1242b52e8ed945271871c8d07a57e6
Aless77 <villaalessandro@outlook.com>
2024-06-03 13:27:36 +0200

    Modif fxml, ajout du back, première requête statistique

--- src/main/java/com/example/rapizz/MainStatistique.java
+++ src/main/java/com/example/rapizz/MainStatistique.java
@@ -2,22 +2,38 @@ package com.example.rapizz;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.SQLException;
 
-public class MainStatistique extends Application{
+public class MainStatistique extends Application {
+
+    public User user;
+
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException, SQLException {
+        Connection cnx = new DataBaseConnect().connect();
+
+        try {
+            this.user = User.getUserById(1, cnx);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        StatistiqueController controller = new StatistiqueController(user, cnx);
+
         FXMLLoader fxmlLoader = new FXMLLoader(MainStatistique.class.getResource("mainStatistique-view.fxml"));
+        fxmlLoader.setController(controller);
+
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
 
-        Connection cnx = new DataBaseConnect().connect();
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/rapizz/StatistiqueController.java
+++ src/main/java/com/example/rapizz/StatistiqueController.java
@@ -3,9 +3,15 @@ package com.example.rapizz;
 import javafx.animation.ScaleTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.util.Duration;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class StatistiqueController {
     @FXML
@@ -29,21 +35,76 @@ public class StatistiqueController {
     @FXML
     private AnchorPane rootPane;
 
+    private User user;
+
+    private Connection cnx;
+
     @FXML
-    public void initialize() {
+    private Label nameUser;
+
+    @FXML
+    private Label firstNameBestClient;
+
+    @FXML
+    private Label lastNameBestClient;
+
+    @FXML
+    private Label soldeDepenseBestClient;
+
+    @FXML
+    private Label nbPizzaCommanderBestClient;
+
+    public StatistiqueController(User user, Connection cnx) {
+        this.user = user;
+        this.cnx = cnx;
+    }
+
+    public void getBestClient() throws SQLException { // Le meilleur client est le client qui a commandé le plus de pizzas
+        String query = "SELECT c.nom, c.prenom, c.solde, SUM(pd.quantite) as nb_pizza, SUM(cmd.prix_total) AS solde_depenser " +
+                "FROM user AS c JOIN commande AS cmd ON c.id_user = cmd.id_client JOIN produitsvendus AS pd ON cmd.id_commande = pd.id_commande " +
+                "GROUP BY c.id_user " +
+                "ORDER BY nb_pizza DESC " +
+                "LIMIT 1;";
+
+
+        // Utilisation de PreparedStatement pour sécuriser la requête
+        try (PreparedStatement pstmt = cnx.prepareStatement(query)) {
+            // Exécution de la requête
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    // Création d'un objet User avec les informations récupérées
+                    firstNameBestClient.setText(rs.getString("nom"));
+                    lastNameBestClient.setText(rs.getString("prenom"));
+                    soldeDepenseBestClient.setText("Solde dépensé : " + rs.getString("solde_depenser") + " €");
+                    nbPizzaCommanderBestClient.setText("Nombre pizza commandé : " + rs.getString("nb_pizza"));
+                }
+            } catch (SQLException e) {
+                System.out.println("Erreur lors de l'exécution de la requête, getBestClient : " + e.getMessage());
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @FXML
+    public void initialize() throws SQLException {
         // Ajouter des gestionnaires d'événements pour le bouton
         deconnexionButton.setOnMousePressed(event -> increaseButtonSize());
         deconnexionButton.setOnMouseReleased(event -> resetButtonSize());
 
         // Ajuster la largeur des HBox
         rootPane.widthProperty().addListener((obs, oldVal, newVal) -> {
-            double newWidth = newVal.doubleValue();
-            clientInfoBox.setPrefWidth(newWidth * 0.45);
-            livreurInfoBox.setPrefWidth(newWidth * 0.45);
-            bestPizzaIngredientBox.setPrefWidth(newWidth * 0.75);
-            vehiculeNoUseInfoBox.setPrefWidth(newWidth * 0.45);
-            statCommandeInfoBox.setPrefWidth(newWidth * 0.45);
+            double newWidth = newVal.doubleValue(); // Nouvelle largeur de la fenêtre
+            clientInfoBox.setPrefWidth(newWidth * 0.45); // Ajuster la largeur de la boîte d'informations client
+            livreurInfoBox.setPrefWidth(newWidth * 0.45); // Ajuster la largeur de la boîte d'informations livreur
+            bestPizzaIngredientBox.setPrefWidth(newWidth * 0.60); // Ajuster la largeur de la boîte d'informations de la meilleure pizza
+            statCommandeInfoBox.setPrefWidth(newWidth * 0.30); // Ajuster la largeur de la boîte d'informations sur les statistiques de commande
         });
+
+        nameUser.setText(user.toString());
+
+        getBestClient();
+
+
     }
 
     // Méthode pour augmenter la taille du bouton
@@ -61,4 +122,5 @@ public class StatistiqueController {
         scaleDown.setToY(1.0);
         scaleDown.play();
     }
+
 }
